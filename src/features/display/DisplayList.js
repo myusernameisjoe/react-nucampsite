ec8b22ebd3f3d04c77f9c3a719fd8e56a48017a7
Joseph Hansberry <jphansberry1@gmail.com>
2024-02-04 14:59:22 -0600

    added AnimatedDisplayCard.js

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,6 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
+// import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
@@ -11,10 +12,15 @@ const DisplayList = () => {
         <Row>
             {items.map((item, idx) => {
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
-                );
+
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
+                )
+
+
             })}
         </Row>
     );
@@ -24,3 +30,6 @@ export default DisplayList;
 
 
 // display a list of featured items
+
+// conditional rendering (wrap Col in item &&())
+// what if data changes and there is no featured campsite (shit explodes)
